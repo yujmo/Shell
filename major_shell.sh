5c684b42c5a3f7983363283a5ca25cef91922485
yujmo <yujmo@users.noreply.github.com>
2017-08-30 16:09:55 +0800

    Update major_shell.sh

--- major_shell.sh
+++ major_shell.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+NODE_IP=`ifconfig | grep 'inet'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $2}' |grep 10.0.2` 
+echo "******"|passwd --stdin root
+echo "nameserver 114.114.114.114" > /etc/resolv.conf 
+rm -rf /etc/yum.repos.d/CentOS-[CDfMSV]*
+yum install yum-utils -y
+yum makecache fast
+
+tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://ld7jqf2j.mirror.aliyuncs.com"]
+}
+EOF
