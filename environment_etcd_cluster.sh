6ab0fe9cc20e6b303db31d38816ce06e0fedfc49
yujmo <yujmo@users.noreply.github.com>
2017-08-30 16:08:27 +0800

    Delete environment_etcd_cluster.sh

--- environment_etcd_cluster.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export NODE_NAME="etcd-""${HOSTNAME}"
-export NODE_IP=`ifconfig | grep 'inet'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $2}' |grep 10.0.2`
-export NODE_IPS="10.0.2.11 10.0.2.12 10.0.2.31"
-echo $NODE_NAME
