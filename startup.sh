ab0c8f7de2e21d919472648821861b51827f0c0e
yujmo <yujmo@users.noreply.github.com>
2017-10-24 22:18:44 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-yum update -y > /dev/null
+os_name=`lsb_release -a |grep Distributor |awk '{print $3}'`
+echo $os_name
+case $os_name in
+        CentOS)
+        yum update -y > /dev/null
+        echo `date`
+        ;;
+        *)
+        echo "Can't get the os name"
+        ;;
+esac
+
 directory=`ls -F |grep '/$'`
 #echo $directory
-for i in $directory
-        do
-        cd /root/$i && git add . && git commit -m "add some files" && git push origin master
-        done
+#for i in $directory
+#       do
+#       cd /root/$i && git add . && git commit -m "add some files" && git push origin master
+#       done
+~
