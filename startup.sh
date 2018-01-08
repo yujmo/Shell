b3881fff16fecc545f2fbc0f8c8af2282fe15cd9
yujmo <yujmo@users.noreply.github.com>
2018-01-08 14:08:08 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -17,4 +17,19 @@ directory=`ls -F |grep '/$'`
 #       do
 #       cd /root/$i && git add . && git commit -m "add some files" && git push origin master
 #       done
-~
+
+
+echo "admin123"|passwd --stdin root
+echo "nameserver 114.114.114.114" > /etc/resolv.conf 
+rm -rf /etc/yum.repos.d/CentOS-[CDfMSV]*
+yum install yum-utils -y
+yum makecache fast
+mkdir /etc/docker
+touch  /etc/docker/daemon.json
+tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://ld7jqf2j.mirror.aliyuncs.com"]
+}
+EOF
+
+echo "hello"
