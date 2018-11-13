35c023d28804fc30db023395879a4024fbe08abf
yujmo <yujmo@users.noreply.github.com>
2018-11-13 10:59:11 +0800

    Update autossh.sh

--- autossh.sh
+++ autossh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #反向代理，将内网主机暴露到具有公网IP的服务器中
-autossh -M 7281 -fCNR 7280:127.0.0.1:80 root@xxx.xxx.xxx.xxx -p 22
+autossh -M 7281 -fCNR 7280:127.0.0.1:22 root@openfaas.cn -p 22
 # 如果不成功，端口被占用，可以去掉-f。还有公钥问题
